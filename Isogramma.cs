21cf7bd77b4558e3656f4968ed38bddcc3c82faa
Alessandro Bartolucci <127590077+ale02082000@users.noreply.github.com>
2023-05-09 16:52:16 +0200

    Add files via upload

---
+++ Isogramma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+
+
+     var alfabeto = new int [26];
+
+
+
+        foreach(var Letter in word.ToLower())
+        {
+
+            
+            if (Char.IsLetter(Letter))
+            {
+                alfabeto [Letter - 'a']++;
+                if (alfabeto[Letter - 'a'] > 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+}
